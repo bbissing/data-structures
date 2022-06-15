8646f6b4999f67956a4487da63603dfa249f4415
Daisy Ding <xd222@cornell.edu>
2022-06-14 19:29:28 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    ///testting
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var oldHead = list.head.value;
+    list.head = list.head.next;
+    return oldHead;
   };
 
   list.contains = function(target) {
+    //recursion
+    var findTarget = function(node, target) {
+      //break the recursion here!
+      if (node.value === target) {
+        return true;
+      } else if (node.next === null) {
+        return false;
+      } else {
+        return findTarget(node.next, target);
+      }
+    };
+    return findTarget(list.head, target);
   };
 
   return list;
